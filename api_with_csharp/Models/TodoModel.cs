b3cb807426ba0920611c99a1781795def260b6c8
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-03-06 21:39:34 +0700

    add: Models

---
+++ api_with_csharp/Models/TodoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace api_with_csharp.Models
+{
+    public class TodoModel
+    {
+        public long id { get; set; }
+        public string title { get; set; }
+        public bool isCompleted { get; set; }
+
+    }
+}
